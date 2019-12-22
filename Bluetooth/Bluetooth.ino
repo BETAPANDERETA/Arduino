0f7b129877f67c454b08061c4b465246e99c43c5
Leonardo Betancur Díaz <49081413+BETAPANDERETA@users.noreply.github.com>
2019-12-22 18:53:13 -0500

    Add files via upload
    Prueba de envío de datos seriales a python

---
+++ Bluetooth/Bluetooth.ino
@@ -0,0 +1,38 @@
+#include <SoftwareSerial.h>
+#include <math.h>
+int rx = 10;
+int tx = 11;
+int info;
+SoftwareSerial miBto(rx, tx);
+
+void setup() {
+  Serial.begin(9600);
+  miBto.begin(9600);
+  Serial.print("--------------------------");
+  Serial.print("\n|PUERTO SERIAL CONECTADO|\n ");
+  Serial.print("-------------------------\n ");
+}
+
+void loop() {
+  if (Serial.available()) {
+    info = Serial.read();
+    if ( info == 49 and info != "") {
+      makeData();
+    }
+    else Serial.print("\nbyte sin procesar\n");
+  }
+}
+void makeData() {
+  int i = 0;
+  double  fx = 0;
+  for (i; i <= 500; i++) {
+    Serial.print("f(");
+    Serial.print(i);
+    Serial.print(")=");
+    fx = pow(i,2);
+    Serial.print(fx);
+    Serial.print("\n");
+    miBto.println(fx);
+  }
+  miBto.print("FIN TRANSMISIÓN");
+}
---
+++ Bluetooth/BluetoothReceptor.py
@@ -0,0 +1,15 @@
+import serial
+port="COM6"
+cont = 0
+lista_datos=[]
+print("||ENCENDIDO||")
+try:
+    bto=serial.Serial(port, 9600)
+    while True:
+        data = bto.readline()
+        print("f("+str(cont)+") = ",data.decode())
+        lista_datos.append(data)
+        cont+=1
+    print(lista_datos)
+except OSError:
+        print("Dispositivo fuera de línea")
