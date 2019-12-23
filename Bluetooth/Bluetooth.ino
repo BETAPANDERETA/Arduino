c50664ac0a22430159717be4cea673b94f5ac62a
Leonardo Betancur Díaz <49081413+BETAPANDERETA@users.noreply.github.com>
2019-12-22 19:06:17 -0500

    Update Bluetooth.ino

--- Bluetooth/Bluetooth.ino
+++ Bluetooth/Bluetooth.ino
@@ -4,7 +4,7 @@
 int rx = 10;
 int tx = 11;
 int info;
-SoftwareSerial miBto(rx, tx);
+SoftwareSerial miBto(rx, tx); //Puertos seriales asignados gracias a la librería SoftwareSerial
 
 void setup() {
   Serial.begin(9600);
@@ -16,14 +16,14 @@ void setup() {
 
 void loop() {
   if (Serial.available()) {
-    info = Serial.read();
-    if ( info == 49 and info != "") {
+    info = Serial.read(); // bytes de información que recibe del puerto serial
+    if ( info == 49 and info != "") { // 49 es la cantidad (Decimal) de bytes que ocupa el caracter numérico "1"
       makeData();
     }
     else Serial.print("\nbyte sin procesar\n");
   }
 }
-
+//Función que crea unos datos para probar la recepción en python
 void makeData() {
   int i = 0;
   double  fx = 0;
