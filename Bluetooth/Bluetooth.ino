5b4c1a86adacfc502a46012a094b024172ae550e
Leonardo Betancur Díaz <49081413+BETAPANDERETA@users.noreply.github.com>
2019-12-24 13:42:11 -0500

    Add files via upload
    V1.0 Nuevas modificaciones de código

--- Bluetooth/Bluetooth.ino
+++ Bluetooth/Bluetooth.ino
@@ -1,10 +1,12 @@
-//HECHO POR LEONARDO BETANCUR A.K.A. BETAPANDERETA 
+/*
+ * HECHO POR BETAPANDERETA; SIMULACIÓN DE ENVÍO DE DATOS A PYTHON POR BLUETOOTH
+*/
 #include <SoftwareSerial.h>
 #include <math.h>
 int rx = 10;
 int tx = 11;
 int info;
-SoftwareSerial miBto(rx, tx); //Puertos seriales asignados gracias a la librería SoftwareSerial
+SoftwareSerial miBto(rx, tx);
 
 void setup() {
   Serial.begin(9600);
@@ -16,14 +18,22 @@ void setup() {
 
 void loop() {
   if (Serial.available()) {
-    info = Serial.read(); // bytes de información que recibe del puerto serial
-    if ( info == 49 and info != "") { // 49 es la cantidad (Decimal) de bytes que ocupa el caracter numérico "1"
-      makeData();
+    info = Serial.read();
+    switch ( info )
+    {
+      case 49 :
+        makeData();
+        break;
+      case 48:
+        miBto.print('0');
+        Serial.println("TRANSMISIÓN IRRUMPIDA");
+        break;
+      default:
+        break;
     }
-    else Serial.print("\nbyte sin procesar\n");
   }
 }
-//Función que crea unos datos para probar la recepción en python
+
 void makeData() {
   int i = 0;
   double  fx = 0;
@@ -31,10 +41,9 @@ void makeData() {
     Serial.print("f(");
     Serial.print(i);
     Serial.print(")=");
-    fx = pow(i,2);
+    fx = pow(i, 2);
     Serial.print(fx);
     Serial.print("\n");
     miBto.println(fx);
   }
-  miBto.print("FIN TRANSMISIÓN");
 }
--- Bluetooth/BluetoothReceptor.py
+++ Bluetooth/BluetoothReceptor.py
@@ -1,16 +1,48 @@
-##HECHO POR LEONARDO BETANCUR A.K.A. BETAPANDERETA
+#HECHO POR LEONARDO BETANCUR A.K.A BETAPANDERETA
 import serial
-port="COM6"
-cont = 0
+import time
+#VARIABLES
+sec = 10
+port = "COM6"
 lista_datos=[]
-print("||ENCENDIDO||")
-try:
-    bto=serial.Serial(port, 9600)
-    while True:
-        data = bto.readline()
-        print("f("+str(cont)+") = ",data.decode())
-        lista_datos.append(data)
-        cont+=1
-    print(lista_datos)
-except OSError:
-        print("Dispositivo fuera de línea")
+#FUNCIONES
+def procesar(info):
+    paso = '\r' 
+    dato = info.split(paso)
+    for i in range (len(dato)):
+        if dato[i] !='n' :
+            return dato[i]
+def convert_data(lista):
+    for i in range (len(lista)):
+        if lista[i] == "\x000.00":
+            lista[i] == "000.00"
+    return lista
+    
+print("\t\t||ENCENDIDO||\n")
+##MAIN}
+def main():
+    cont = 0
+    try:
+        bto=serial.Serial(port, 9600)
+        while True:
+            while bto.inWaiting()== False:
+                print("CONECTANDO ...")
+                time.sleep(sec)
+                if bto.inWaiting()== True:
+                    print("¡CONECTADO!")
+                    continue
+            data = bto.readline()
+            data_dec = data.decode()
+            if data == '0':
+                print("FIN TRANSMISIÓN")
+                break
+            else: 
+                print("f("+str(cont)+") = ",data_dec)
+                lista_datos.append(procesar(data_dec))
+                cont+=1
+        if len(lista_datos)!= 0:
+            print(convert_data(lista_datos))
+    except OSError:
+            print("Dispositivo fuera de línea")
+            print(lista_datos)
+main()
